3e4e19d391933f1135003125a2b0d3e854bfabea
Jacob Fox <jacob.jmf@gmail.com>
2018-07-09 23:03:58 -0400

    updating help command

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,8 @@ exports.run = (client, message, args) => {
 	botresponse += 'Returns the all important list of first leaders.\n';
 	botresponse += '**!last**\n';
 	botresponse += 'Returns the last recorded first.\n';
+	botresponse += '**!me**\n';
+	botresponse += 'Returns all of your recorded firsts.\n';
 
 	message.channel.send('DM Sent!');
 	message.author.send(botresponse);
