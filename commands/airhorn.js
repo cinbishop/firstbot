eb0061ebb11ecc81896efd1878f0a15f6494dfa0
Jacob Fox <jacob.jmf@gmail.com>
2019-04-24 20:48:42 -0400

    adding new schmeckle sink

---
+++ commands/airhorn.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, args) => {
+
+	const key = message.author.id;
+	const user = message.member.displayName;
+
+	const hasFirsts = client.firstdata.has(key);
+
+	const totalCoins = hasFirsts ? client.firstdata.getProp(key,'coins') : 0;
+
+	client.firstdata.setProp(key, 'coins', totalCoins-100);
+
+	const botresponse = `The pact is sealed, ${user}, you have **${totalCoins - 100}** schmeckle${totalCoins-100 > 1 ? 's' : ''} remaining. https://www.youtube.com/watch?v=QVw5mnRI8Zw`;
+
+	message.channel.send(botresponse);
+
+}
